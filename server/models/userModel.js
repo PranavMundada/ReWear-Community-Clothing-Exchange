7c12b564dba6fb939e5a95ff3667e3de3774e403
PranavMundada <pjmundada2005@gmail.com>
2025-07-12 10:37:37 +0530

    Added User Auth with JWT

---
+++ server/models/userModel.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcryptjs, { compare } from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please tell your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please tell your email "],
+    unique: true,
+    validate: [validator.isEmail, "Please provide a valid email "],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Password are not same",
+    },
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcryptjs.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.comparePassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  const same = await bcryptjs.compare(candidatePassword, userPassword);
+  return same;
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  // False means NOT changed
+  return false;
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+import express from "express"
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+import userRouter from "./routes/userRoute.js"
+
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const app = express();
 
 app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
@@ -16,6 +22,4 @@ mongoose.connect(process.env.MONGO_URI.replace('<db_password>',process.env.DB_PA
   .catch(err => console.log(err));
 
 // Sample route
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+app.use("/api/users",userRouter);
